f998fd1cc8232fe444907da1b8d1a009f74a79b9
Valentin <>
2025-10-24 16:28:04 +0800

    Добавлены слои для домашней и админ страницы, прописаны пути

--- resources/js/constants/routes.ts
+++ resources/js/constants/routes.ts
@@ -1,3 +1,4 @@
 export const ROUTES = {
     ROOT: "/",
+    ADMIN: "admin",
 } as const;
--- resources/js/services/routes/AppRoutes.tsx
+++ resources/js/services/routes/AppRoutes.tsx
@@ -1,11 +1,28 @@
 import { createBrowserRouter } from "react-router";
 import { ROUTES } from "@constants/routes";
+import HomeLayout from "@/layouts/HomeLayout";
+import AdminLayout from "@/layouts/AdminLayout";
 import Home from "@pages/Home/Home";
+import Admin from "@/pages/Admin/Admin";
 
 const router = createBrowserRouter([
     {
-        path: ROUTES.ROOT,
-        element: <Home />,
+        element: <HomeLayout />,
+        children: [
+            {
+                path: ROUTES.ROOT,
+                element: <Home />,
+            },
+        ],
+    },
+    {
+        element: <AdminLayout />,
+        children: [
+            {
+                path: ROUTES.ADMIN,
+                element: <Admin />,
+            },
+        ],
     },
 ]);
 
